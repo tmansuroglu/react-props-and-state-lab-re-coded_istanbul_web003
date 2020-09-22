c17480114f87f6bc73998e31992a177d08ed39f8
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-09-22 16:26:22 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,43 @@ class App extends React.Component {
     }
   }
 
+  fetchData = () => {
+    const URL = this.state.filters.type === "all" ? "/api/pets" : `/api/pets?type=${this.state.filters.type}`
+
+    fetch(URL)
+      .then(resp => resp.json())
+      .then(data => {
+        this.setState({
+          ...this.state,
+          pets: data
+        })
+
+      })
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  handleChange = async (event) => {
+
+    this.setState({
+      ...this.state,
+      filters: { type: event.target.value }
+    })
+
+  }
+
+  handleAdoptPet = (id) => {
+    const pet = this.state.pets.find(x => x.id === id)
+    const petIndex = this.state.pets.indexOf(pet)
+    this.state.pets[petIndex] = { ...this.state.pets[petIndex], isAdopted: true }
+    this.setState({
+      ...this.state
+    })
+
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +61,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleChange} onFindPetsClick={this.fetchData} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.handleAdoptPet} pets={this.state.pets} />
             </div>
           </div>
         </div>
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,22 +1,28 @@
 import React from 'react'
 
 class Filters extends React.Component {
+  constructor() {
+    super()
+  }
+
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={this.props.onChangeType}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
             <option value="micropig">Micropigs</option>
           </select>
+          <div className="field">
+            <button onClick={this.props.onFindPetsClick} className="ui secondary button">Find pets</button>
+          </div>
         </div>
 
-        <div className="field">
-          <button className="ui secondary button">Find pets</button>
-        </div>
+
       </div>
     )
   }
